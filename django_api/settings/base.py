beccede7837910e7b4b1c2460ce0dda72c1eb313
Eddy <eddy15201@gmail.com>
2021-09-02 15:00:02 +0800

    update base

--- django_api/settings/base.py
+++ django_api/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 print(f"BASE_DIR: {BASE_DIR}")
 print(f"Path(__file__).resolve(): {Path(__file__).resolve()}")
 
--- django_api/settings/deploy_heroku.py
+++ django_api/settings/deploy_heroku.py
@@ -11,8 +11,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # 設定靜態檔位置。
-# STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 設定資料庫。
 DATABASES = {
