94e50375d69d566f0311b5d9c43571eacbbb3a50
Eddy <eddy15201@gmail.com>
2021-09-04 21:05:17 +0800

    update test

--- django_api/settings/deploy_heroku.py
+++ django_api/settings/deploy_heroku.py
@@ -13,6 +13,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # 設定靜態檔位置。
 # STATIC_ROOT = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 # 設定資料庫。
 DATABASES = {
